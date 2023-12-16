f6cc2b6cfceb0125e7f84a982cc6c8da66fbf840
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2023-12-16 20:45:15 +0530

    added the controller and route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -11,8 +13,13 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app = express() 
+app.use(express.json())
 
 
 app.listen(3400,()=>{
     console.log("server started in 3400 ")
 })
+
+
+app.use("/api/user",userRoutes)
+app.use("/api/auth",authRoutes)
